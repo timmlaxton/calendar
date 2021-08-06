ab881cfcbbe0c72c43bb23452e107c2d507fa99c
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-06 14:04:46 +0100

    added year in js

---
+++ script.js
@@ -0,0 +1,27 @@
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem("events")
+  ? JSON.parse(localStorage.getItem("events"))
+  : [];
+
+const calendar = document.getElementById("calendar");
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function load() {
+  const dt = new Date();
+
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
+  console.log(day, month, year);
+}
+
+load();
