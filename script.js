9ce59e9ef78807cc1737d12e5b04e16fde0600fb
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-06 14:23:34 +0100

    displaying Month to calendar

--- script.js
+++ script.js
@@ -34,6 +34,11 @@ function load() {
   });
   const paddingDays = weekdays.indexOf(dateString.split(",")[0]);
 
+  document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
+    "en-uk",
+    { month: "long" }
+  )} ${year}`;
+
   for (let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement("div");
     daySquare.classList.add("day");
