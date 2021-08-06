edc79b38f470ba67fac9dfea4d37397994887fec
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-08-06 14:19:01 +0100

    rendered calendar to screen

--- script.js
+++ script.js
@@ -21,7 +21,32 @@ function load() {
   const day = dt.getDate();
   const month = dt.getMonth();
   const year = dt.getFullYear();
-  console.log(day, month, year);
+
+  const firstDayOfMonth = new Date(year, month, 1);
+
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  const dateString = firstDayOfMonth.toLocaleDateString("en-uk", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+  const paddingDays = weekdays.indexOf(dateString.split(",")[0]);
+
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement("div");
+    daySquare.classList.add("day");
+
+    if (i > paddingDays) {
+      daySquare.innerText = i - paddingDays;
+      daySquare.addEventListener("click", () => console.log("click"));
+    } else {
+      daySquare.classList.add("padding");
+    }
+
+    calendar.appendChild(daySquare);
+  }
 }
 
 load();
